46093160c31a38804b560271a307122e324e208b
xQberto777x <r.carloscanales@yahoo.es>
2022-04-12 18:04:02 -0600

    perrito ya puse el comentario

--- Prueba_y_error.js
+++ Prueba_y_error.js
@@ -1,3 +1,13 @@
 // angel es homo.
 /* angel hp se la come*/
 var myName;
+myName = 7;
+var Name;
+Name = "qberto"
+var myVar;
+myvar =5;
+console.log(myName)
+var a;
+a = 7;
+console.log(a)
+console.log(Name)
